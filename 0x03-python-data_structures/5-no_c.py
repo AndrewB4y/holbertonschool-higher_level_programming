7b053476a94f83927dc2bc8f7853a96f2a731037
AndrewB4y <andresbaymon@gmail.com>
2020-05-06 02:34:07 +0000

    Fixing 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,11 @@
 
 
 def no_c(my_string):
-    my_string = my_string.replace('c', '')
-    my_string = my_string.replace('C', '')
+    str_l = len(my_string)
+    for idx in range(str_l - 1, -1, -1):
+        if my_string[idx] == 'c' or my_string[idx] == 'C':
+            if idx + 1 >= len(my_string):
+                my_string = my_string[:idx]
+            else:
+                my_string = my_string[:idx] + my_string[idx + 1:]
     return my_string
