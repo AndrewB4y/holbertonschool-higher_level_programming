ef692754521f15c5d9c7f38e22d4381636cf6e29
AndrewB4y <andresbaymon@gmail.com>
2020-05-19 01:25:50 +0000

    Changing file 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,15 +3,12 @@
 
 def safe_print_list(my_list=[], x=0):
     p = 0
-    try:
-        for p, e in enumerate(my_list[:x]):
-            print("{}".format(e), end='')
-        p = p + 1
-        print()
-    except:
-        for p, e in enumerate(my_list[:]):
-            print("{}".format(e), end='')
-        p = p + 1
-        print()
-    finally:
-        return p
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+            p = p + 1
+        except:
+            break
+
+    print()
+    return p
