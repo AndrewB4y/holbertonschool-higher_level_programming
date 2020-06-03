db42a302156526825748a1c93d4b6bf05a1385ed
AndrewB4y <andresbaymon@gmail.com>
2020-06-03 02:27:18 +0000

    Adding the file 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" add_attribute module """
+
+
+def add_attribute(obj, att_name, att_value):
+    if type(obj).__module__ != "builtins":
+        setattr(obj, att_name, att_value)
+    else:
+        raise TypeError("can't add new attribute")
