61feb7fe63e790dc6480ddc033726727f1f0e84e
AndrewB4y <andresbaymon@gmail.com>
2020-06-03 02:49:32 +0000

    101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,7 +6,7 @@
 def add_attribute(obj, att_name, att_value):
     """ add_attribute function """
 
-    if obj.__class__.__module__ == "builtins":
+    if obj.__class__.__module__ == 'builtins':
         raise TypeError("can't add new attribute")
 
     setattr(obj, att_name, att_value)
