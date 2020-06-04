248984aa80ca0b84a79e182bd5e489103e297f8a
AndrewB4y <andresbaymon@gmail.com>
2020-06-04 01:05:29 +0000

    Adding the file 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,10 +7,20 @@ if __name__ == "__main__":
 
     import sys
     import json
+    import os
 
-    save_to_json_file = __import__('7-save_to_json_file')
-    load_from_json_file = __import__('8-load_from_json_file')
+    f_n = '7-save_to_json_file'
+    save_to_json_file = __import__(f_n).save_to_json_file
+    f_n = '8-load_from_json_file'
+    load_from_json_file = __import__(f_n).load_from_json_file
 
     input = sys.argv
     to_save = input[1:]
-    j_file = load_from_json_file('add_item.json')
+    f_name = "add_item.json"
+    if not os.access(f_name, os.R_OK):
+        save_to_json_file(to_save, f_name)
+        exit()
+
+    j_file = load_from_json_file(f_name)
+    j_file.extend(to_save)
+    save_to_json_file(j_file, f_name)
