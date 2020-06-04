fff64645ad9d03ef8c7dcc062422cdbbecb6b71c
AndrewB4y <andresbaymon@gmail.com>
2020-06-04 00:36:10 +0000

    Fixing output for files 0 and 2, having extra new line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,5 @@ def read_file(filename=""):
     reads a text file (UTF8) and prints it to stdout
     """
 
-    with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read())
+    with open(filename, mode='r', encoding='utf-8') as f:
+        print(f.read(), end='')
--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,11 +7,13 @@ def read_lines(filename="", nb_lines=0):
     """
     read_lines function
 
-    reads n lines of a text file (UTF8) and prints it to stdout
+    Reads n lines of a text file (UTF8) and prints it to stdout
+    Reads the entire file if @nb_lines is lower or equal to 0 OR
+    greater or equal to the total number of lines of the file
     """
     with open(filename, mode='r', encoding='utf-8') as f:
         if nb_lines <= 0:
-            print(f.read())
+            print(f.read(), end='')
 
         for n, line in enumerate(f.readlines()):
             if n >= nb_lines:
