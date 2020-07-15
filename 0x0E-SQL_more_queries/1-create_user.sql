6688363d9363b3b5940e5f864599c81e49fc3475
AndrewB4y <andresbaymon@gmail.com>
2020-07-15 02:47:14 +0000

    Fizing file 1, password was not setted right

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- 1-create_usercreates the MySQL server user user_0d_1.
-GRANT ALL ON *.* TO user_0d_1@localhost IDENTIFIED BY 'password';
+GRANT ALL ON *.* TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
