4d74cbb1a619e3b10971b345166853e72bdfa21b
AndrewB4y <andresbaymon@gmail.com>
2020-07-15 03:33:56 +0000

    Fixing order in output of file 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,5 +1,4 @@
--- 100-not_my_genres list all genres not linked to
--- the show Dexter.
+-- 100-not_my_genres list all genres not linked to the show Dexter.
 SELECT DISTINCT tv_genres.name AS name
 FROM tv_shows, tv_genres, tv_show_genres
 WHERE tv_genres.name NOT IN(
@@ -8,5 +7,5 @@ WHERE tv_genres.name NOT IN(
       WHERE tv_shows.title="Dexter"
       AND tv_shows.id=tv_show_genres.show_id
       AND tv_show_genres.genre_id=tv_genres.id
-      ORDER BY tv_genres.name ASC
-);
+)
+ORDER BY tv_genres.name ASC;
