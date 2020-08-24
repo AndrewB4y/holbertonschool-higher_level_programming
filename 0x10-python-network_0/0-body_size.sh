da8b3c9f72dc9d4d731bc76934723ffb5399cef1
AndrewB4y <andresbaymon@gmail.com>
2020-08-24 21:17:48 +0000

    Considering more fields on cut for file 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sw ' %{size_download}' "$1" | cut -d ' ' -f3
+curl -sw ' %{size_download}' "$1" | rev | cut -d ' ' -f1 | rev
