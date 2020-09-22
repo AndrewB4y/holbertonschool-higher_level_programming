d7986f68d8e4a7558b2b0d244430fad1fc40f56a
AndrewB4y <andresbaymon@gmail.com>
2020-09-22 03:50:40 +0000

    Fixing output on file 9: missing a space between output's colon and message

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -4,6 +4,6 @@ exports.logMe = function (item) {
   if (typeof this.i === 'undefined') {
     this.i = 0;
   }
-  console.log(this.i + ':' + item);
+  console.log(this.i + ': ' + item);
   this.i++;
 };
